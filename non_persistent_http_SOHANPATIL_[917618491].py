25d36afd1d9c58ed36765e959b548feb5eae2980
Sohan Patil <sohanpatil@campus-013-209.ucdavis.edu>
2022-05-09 09:26:25 -0700

    HTTP works

--- non_persistent_http_SOHANPATIL_[917618491].py
+++ non_persistent_http_SOHANPATIL_[917618491].py
@@ -10,31 +10,34 @@ import os
 import time
 import regex as re
 
-f = open("ecs152a.html", "a")
-def retrieveHTML(server_name,server_port, file_size, clientSocket, request):
-    
-    targetFileSize = 58533
-    targetResponse = ''
-    clientSocket.send(request.encode('utf-8'))
 
+def retrieveHTML(server_name,server_port, file_size, clientSocket, request):
+
+    f = open("ecs152a.html", "a")    
+    targetFileSize = 58533
+    totalResponse = ''
+    clientSocket.send(request.encode('utf-8'))
+    response = clientSocket.recv(4096)
+    content = response.decode('utf-8').split("\r\n\r\n")
+    totalResponse+= content[1]
+    file_size = len(totalResponse)
     while(file_size < targetFileSize):
         response = clientSocket.recv(4096)
-        targetResponse += response
-        decodedResponse = response.decode('utf-8')
-        decodedResponseLength = len(decodedResponse.encode('utf-8'))
+        
+        totalResponse += response.decode('utf-8')
 
-        if(file_size + decodedResponseLength <= targetFileSize):
-            percentageRecieved = (file_size + decodedResponseLength)/targetFileSize * 100
-            f.write(response.decode('utf-8'))
-            print("received %: ",round(percentageRecieved))
-
-        else:
-            break
-        file_size = os.path.getsize('./ecs152a.html')
+        percentageRecieved = file_size/targetFileSize * 100
+        print("received %: ",round(percentageRecieved))
+        file_size = len(totalResponse)
         if(file_size >= targetFileSize):
             break
+                
+    f.write(totalResponse)
+    percentageRecieved = file_size/targetFileSize * 100
+    print("received %: ",round(percentageRecieved))
     
     print("The file size is : {}".format(file_size))
+    f.close()
 
 def parseHTML():
     with open("./ecs152a.html") as page:
@@ -55,18 +58,22 @@ def downloadPictures(imageList):
     
     time.sleep(2)
     for imagePath in imageList:
+        
+        imageSocket = socket(AF_INET, SOCK_STREAM)
+        imageSocket.connect((server_name, server_port))
+
         if "http" in imagePath:
+            imageSocket.close()
             continue
         
         photoName = (imagePath.split("/"))[1]
-        print("Downloading: ",photoName)
         f = open(imagePath,"wb")
 
-        request = "GET /" + imagePath + " HTTP/1.1\r\nHost:173.230.149.18:23662\r\nX-Client-project: project-152A-part2\r\nConnection: keep-alive\r\n\r\n"
+        request = "GET /" + imagePath + " HTTP/1.1\r\nHost:173.230.149.18:23662\r\nX-Client-project: project-152A-part2\r\nConnection: close\r\n\r\n"
         
-        clientSocket.send(request.encode('utf-8'))# Initial request for content length
+        imageSocket.send(request.encode('utf-8'))# Initial request for content length
         totalResponse = b''
-        response = clientSocket.recv(4096)
+        response = imageSocket.recv(4096)
         content = response.split(b"\r\n\r\n")
         totalResponse+= content[1]
         fileSize = len(totalResponse)
@@ -74,29 +81,23 @@ def downloadPictures(imageList):
         if match:
             ans = re.split(b': ',match.group())
             targetFileSize = int(ans[1])
-            print("Found file size")
 
         while fileSize < targetFileSize:
-            response = clientSocket.recv(4096)
+            response = imageSocket.recv(4096)
             
             totalResponse += response
             # fileSize = os.path.getsize(os.path.join(current_directory,photoName))
             fileSize = len(totalResponse)
             if(fileSize >= targetFileSize):
                 break
-            # print("Image Size for {} is {}".format(photoName, fileSize))
-        
-        # matches = totalResponse.split(b"\r\n\r\n")
-        # writeToFile = matches[1]
-        # print("This is the answer:{}".format(writeToFile))
 
         f.write(totalResponse)
         f.close()
 
-        # print("This is i: ", i)
-        # print("This is the totalResponse: \n", totalResponse)
-        # print("This is the response: \n", response)
+
         print("Image: ",photoName," downloaded")
+        imageSocket.close()
+        
     clientSocket.close()
 
 
@@ -108,7 +109,7 @@ file_size = 0
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((server_name, server_port))
 
-request = "GET /ecs152a.html HTTP/1.1\r\nHost:173.230.149.18:23662\r\nX-Client-project: project-152A-part2\r\nConnection: keep-alive\r\n\r\n"
+request = "GET /ecs152a.html HTTP/1.1\r\nHost:173.230.149.18:23662\r\nX-Client-project: project-152A-part2\r\nConnection: close\r\n\r\n"
 
 
 #Functions
